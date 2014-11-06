0846fa2b838c9b768c46cbc341228d3cb877136a
Daniel Steiner <dapstr@gmail.com>
2014-11-06 21:32:33 +0100

    Added and activated custom header feature

--- header.php
+++ header.php
@@ -24,6 +24,11 @@
 
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
+            <?php if ( get_header_image() ) : ?>
+            <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+                <img src="<?php header_image(); ?>" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="">
+            </a>
+            <?php endif; // End header image check. ?>
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</div><!-- .site-branding -->
